c5748da0679367a5f420f0f1004b98d633480bb0
lubaiukang <332138298@qq.com>
2018-02-27 13:54:07 +0800

    update

---
+++ 94_基于request cache请求缓存技术优化批量商品数据查询接口/代码/eshop-cache-ha/src/main/java/com/roncoo/eshop/cache/ha/hystrix/command/GetProductInfoCommand.java
@@ -0,0 +1,45 @@
+package com.roncoo.eshop.cache.ha.hystrix.command;
+
+import com.alibaba.fastjson.JSONObject;
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandKey;
+import com.netflix.hystrix.HystrixThreadPoolKey;
+import com.netflix.hystrix.HystrixThreadPoolProperties;
+import com.roncoo.eshop.cache.ha.http.HttpClientUtils;
+import com.roncoo.eshop.cache.ha.model.ProductInfo;
+
+/**
+ * 获取商品信息
+ * @author Administrator
+ *
+ */
+public class GetProductInfoCommand extends HystrixCommand<ProductInfo> {
+
+	private Long productId;
+	
+	public GetProductInfoCommand(Long productId) {
+		super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("ProductInfoService"))
+				.andCommandKey(HystrixCommandKey.Factory.asKey("GetProductInfoCommand"))
+				.andThreadPoolKey(HystrixThreadPoolKey.Factory.asKey("GetProductInfoPool"))
+				.andThreadPoolPropertiesDefaults(HystrixThreadPoolProperties.Setter()
+						.withCoreSize(15)
+						.withQueueSizeRejectionThreshold(10))
+				);  
+		this.productId = productId;
+	}
+	
+	@Override
+	protected ProductInfo run() throws Exception {
+		String url = "http://127.0.0.1:8082/getProductInfo?productId=" + productId;
+		String response = HttpClientUtils.sendGetRequest(url);
+		System.out.println("调用接口，查询商品数据，productId=" + productId); 
+		return JSONObject.parseObject(response, ProductInfo.class);  
+	}
+	
+	@Override
+	protected String getCacheKey() {
+		return "product_info_" + productId;
+	}
+	
+}
